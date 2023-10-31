80616781962f6383c8b9bc898189b7d4ee81af37
Psihioga <psihioga@mail.ru>
2023-11-01 01:33:04 +0300

    разгрести это дерьмо

--- app/(routes)/transport/page.tsx
+++ app/(routes)/transport/page.tsx
@@ -1,7 +1,93 @@
+"use client";
+
+import { GetData, GetBlobData } from "@/api/getData";
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+
 export default () => {
+  const effectRan = useRef(false);
+  const [data, setData] = useState([{ label: "Nothing here" }]);
+
+  useEffect(() => {
+    if (effectRan.current) {
+      console.log("effect Ran");
+      const getData = async () => {
+        const response: any = await GetBlobData();
+        console.log(response);
+        setData(response);
+      };
+
+      getData();
+    }
+    return () => {
+      console.log("unmounted");
+      effectRan.current = true;
+    };
+  }, []);
+
+  const Table = (props: {}): React.ReactElement => {
     return (
-        <div>
-            Транспорт
-        </div>
+      <table className="w-full border-separate border-spacing-2 border border-slate-500">
+        <tbody>
+          <tr className="">
+            <td className="border border-slate-600">location </td>
+            <td className="border border-slate-600">item_id </td>
+            <td className="border border-slate-600">quality </td>
+          </tr>
+          {data.map((el: any) => {
+            const firstRow = (
+              <tr className="" key={el.location + el.item_id + el.quality}>
+                <td className="border border-slate-600">
+                  {el.location}
+                </td>
+                <td className="border border-slate-600">
+                 {el.item_id}
+                </td>
+                <td className="border border-slate-600">
+                 {el.quality}
+                </td>
+              </tr>
+            );
+
+            if (el.data) {
+              const secondRow = el.data.map((elem: any) => {
+                return (
+                  <tr key={elem.timestamp}>
+                    <td className="border border-slate-600">
+                      {elem.item_count}
+                    </td>
+                    <td className="border border-slate-600">
+                      {elem.avg_price}
+                    </td>
+                    <td className="border border-slate-600">
+                      {elem.timestamp}
+                    </td>
+                  </tr>
+                );
+              });
+              return (
+                <React.Fragment key={Math.random()}>
+                  {firstRow}
+                  <tr>
+                    <td>кол-во </td>
+                    <td>средняя цена </td>
+                    <td>время </td>
+                  </tr>
+                  {secondRow}
+                </React.Fragment>
+              );
+            }
+
+            return firstRow;
+          })}
+        </tbody>
+      </table>
     );
-}
+  };
+  return (
+    <div>
+      <h1>Транспорт</h1>
+      <Table />
+    </div>
+  );
+};
--- app/_api/getData.ts
+++ app/_api/getData.ts
@@ -1,9 +1,133 @@
-type dataType = {
-    label: string
+import { blob } from "./blob";
+type Item = {
+  Index: string;
+  Name: string;
+  UniqueName: string;
+};
+
+type dataType = Item[];
+
+const url = "https://west.albion-online-data.com/api/v2/stats/history/";
+const items = "T4_RUNE,T5_RUNE,T6_RUNE,T7_RUNE,T8_RUNE";
+const date = "date=10-30-2023&end_date=10-31-2023";
+const qualities = "qualities=0";
+const timescale = "time-scale=6";
+
+const locations: Item[] = [
+  {
+    Index: "4002",
+    Name: "FortSterling",
+    UniqueName: "Fort Sterling Market",
+  },
+  {
+    Index: "3008",
+    Name: "Martlock",
+    UniqueName: "Martlock Market",
+  },
+  {
+    Index: "1002",
+    Name: "Lymhurst",
+    UniqueName: "Lymhurst Market",
+  },
+  {
+    Index: "0007",
+    Name: "Thetford",
+    UniqueName: "Thetford Market",
+  },
+
+  {
+    Index: "3005",
+    Name: "Caerleon",
+    UniqueName: "Caerleon Market",
+  },
+  {
+    Index: "5003",
+    Name: "Brecilien",
+    UniqueName: "Brecilien Market",
+  },
+];
+
+const otherLocations = [
+  {
+    Index: "3013-Auction2",
+    UniqueName: "Caerleon Market",
+  },
+];
+
+const ore = [
+  {
+    Index: "917",
+    Name: "T2_ORE",
+    UniqueName: "Copper Ore",
+    Translation: "титановая руда",
+  },
+  {
+    Index: "918",
+    Name: "T3_ORE",
+    UniqueName: "Tin Ore",
+    Translation: "Оловяная руда",
+  },
+  {
+    Index: "919",
+    Name: "T4_ORE",
+    UniqueName: "Iron Ore",
+    Translation: "железная руда",
+  },
+  {
+    Index: "920",
+    Name: "T5_ORE",
+    UniqueName: "Titanium Ore",
+    Translation: "титановая руда",
+  },
+  {
+    Index: "921",
+    Name: "T6_ORE",
+    UniqueName: "Runite Ore",
+    Translation: "рунитовая руда",
+  },
+  {
+    Index: "922",
+    Name: "T7_ORE",
+    UniqueName: "Meteorite Ore",
+    Translation: "метеоритная руда",
+  },
+  {
+    Index: "923",
+    Name: "T8_ORE",
+    UniqueName: "Adamantium Ore",
+    Translation: "Адамантиновая руда",
+  },
+];
+
+function filterData(data: unknown) {
+  return data;
 }
 
+export async function GetData(params: dataType) {
+  const locationsList = locations
+    .reduce((acc, cur) => acc + cur.Name + ",", "")
+    .slice(0, -1);
+  console.log("starting fetch");
+  console.log(
+    `${url}${items}?${date}&${locationsList}&${qualities}&${timescale}`
+  );
+  const response = await fetch(`${url}${items}?${date}`);
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch data: ${response.status} ${response.statusText}`
+    );
+  }
 
+  return filterData(response.json());
+}
 
-export function GetData(params:dataType): [] {
-    return []
+export async function GetBlobData(params: dataType | void) {
+  console.log("starting fetch");
+  const data = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(blob);
+    }, 150);
+  });
+
+  return data;
 }
